d03d83ec9e96ca7fa61ff5285c8c201813024ba2
Zehua-Chen <31496190+zehuac2@users.noreply.github.com>
2021-07-05 14:11:39 -0500

    [feat] Update Formulae

--- Formula/latextools.rb
+++ Formula/latextools.rb
@@ -3,11 +3,20 @@ require 'fileutils'
 class Latextools < Formula
   desc "Latex tools"
   homepage "https://github.com/Zehua-Chen/latextools"
-  url "https://github.com/Zehua-Chen/latextools/releases/download/0.1.0/latextools-osx-x64.zip"
-  sha256 "87d5c1e2788b7ac83c1c6fb6d10aedaa97101c224cacad36b35d98cc9243b28d"
+  url "https://github.com/Zehua-Chen/latextools/archive/refs/tags/0.5.1.zip"
+  sha256 "d13b91bc8eb062f4ee4cd9ddea8c4a18088033d6a67be61ee6420aba942a4c15"
+  license "MIT"
+
+   depends_on "dotnet-sdk" => :build
 
   def install
-    FileUtils.cp_r ".", "#{prefix}"
+    Dir.chdir("src/latextools")
+
+    system "dotnet", "publish", "-c", "Release", "-r", "osx-x64"
+    Dir.mkdir "unzip"
+    FileUtils.mv "latextools-osx-x64.zip" "unzip/latextools-osx-x64.zip"
+    system "unzip", "unzip/latextools-osx-x64.zip"
+    FileUtils.cp_r "unzip/", "#{prefix}"
     bin.install_symlink "#{prefix}/latextools"
   end
 
